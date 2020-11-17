99f1c047dcbc4ed6ad7a442f1e0252020e79bf75
jbessode <jbessode@etud.u-pem.fr>
2020-11-17 09:40:53 +0100

    add dependency + more Vars/Fmls

--- logicFormula/src/Data/Logic/Fml/Some.hs
+++ logicFormula/src/Data/Logic/Fml/Some.hs
@@ -1,6 +1,7 @@
 module Data.Logic.Fml.Some (
   -- * Testing Var Int
   fml1
+  , fml2
 ) where
 
   import qualified Data.Logic.Var      as Var
@@ -12,3 +13,14 @@ module Data.Logic.Fml.Some (
   -- []
   fml1 :: Fml.Fml Int
   fml1 = Fml.Final $ Var.mk 1
+
+  fml2 :: Fml.Fml String
+  fml2 = f
+    where
+      f  = g `Fml.Imply` h
+      g  = g1 `Fml.Equiv` g2
+      g1 = x1 `Fml.NAnd` Fml.Not x2
+      g2 = x3 `Fml.XOr` x4
+      h  = Fml.Not (h1 `Fml.NOr` h2)
+      h1 = x5 `Fml.Or` x6
+      h2 = x7 `Fml.Equiv` Fml.Not x8
--- logicFormula/src/Data/Logic/Var/Some.hs
+++ logicFormula/src/Data/Logic/Var/Some.hs
@@ -1,9 +1,41 @@
 module Data.Logic.Var.Some (
   -- * Testing Var Int
-  var1
+  x1
+, x2
+, x3
+, x4
+, x5
+, x6
+, x7
+, x8
+, x9
 ) where
 
-  import qualified Data.Logic.Var      as Var
+import qualified Data.Logic.Var as Var
 
-  var1 :: Var.Var Int
-  var1 = Var.mk 1
+x1 :: Var.Var String
+x1 = Var.mk "x1"
+
+x2 :: Var.Var String
+x2 = Var.mk "x2"
+
+x3 :: Var.Var String
+x3 = Var.mk "x3"
+
+x4 :: Var.Var String
+x4 = Var.mk "x4"
+
+x5 :: Var.Var String
+x5 = Var.mk "x5"
+
+x6 :: Var.Var String
+x6 = Var.mk "x6"
+
+x7 :: Var.Var String
+x7 = Var.mk "x7"
+
+x8 :: Var.Var String
+x8 = Var.mk "x9"
+
+x9 :: Var.Var String
+x9 = Var.mk "x9"
