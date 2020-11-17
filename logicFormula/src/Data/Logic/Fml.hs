1a6d73234a172f0dd40ae36285d15fbe3db702b5
jbessode <jbessode@etud.u-pem.fr>
2020-11-17 10:16:20 +0100

    vars

--- logicFormula/src/Data/Logic/Fml.hs
+++ logicFormula/src/Data/Logic/Fml.hs
@@ -3,8 +3,8 @@ module Data.Logic.Fml (
   Fml (..)
 
   -- * Querying
-  --, depth
-  --, vars
+  , depth
+  , vars
 
   -- * Formatting
   , prettyFormat
@@ -25,6 +25,7 @@ module Data.Logic.Fml (
 ) where
 
 import qualified Data.Logic.Var as Var
+import qualified Data.List      as L
 
 data Fml a = And   (Fml a) (Fml a)
            | NAnd  (Fml a) (Fml a)
@@ -61,4 +62,31 @@ prettyFormat (Equiv p q) = "(" ++ prettyFormat p ++ " <=> " ++ prettyFormat q ++
 prettyFormat (Not   p)   = "-" ++ prettyFormat p
 prettyFormat (Final v)   = show v
 
--- to be completed...
+-- |’vars’ @p@ returns all variables that occur in formula @p@. Duplicate
+--  occurrences are removed.
+vars :: (Eq a) => Fml a -> [Var.Var a]
+vars f = L.nub $ getVars f
+  where
+  	getVars (Final p) = [p]
+  	getVars (Not   p)   = [] ++ getVars p
+  	getVars (And   p q) = [] ++ getVars p ++ getVars q
+  	getVars (NAnd  p q) = [] ++ getVars p ++ getVars q
+  	getVars (Or    p q) = [] ++ getVars p ++ getVars q
+  	getVars (NOr   p q) = [] ++ getVars p ++ getVars q
+  	getVars (XOr   p q) = [] ++ getVars p ++ getVars q
+  	getVars (XNOr  p q) = [] ++ getVars p ++ getVars q
+  	getVars (Imply p q) = [] ++ getVars p ++ getVars q
+  	getVars (Equiv p q) = [] ++ getVars p ++ getVars q
+
+-- |’depth’ @p@ returns the depth of fomula @p@.
+depth :: (Num b, Ord b) => Fml a -> b
+depth (Final p) = 0
+depth (Not   p) = 0
+depth (And   p q) = 1 + max $ depth p $ depth q
+depth (NAnd  p q) = 1 + max $ depth p $ depth q
+depth (Or    p q) = 1 + max $ depth p $ depth q
+depth (NOr   p q) = 1 + max $ depth p $ depth q
+depth (XOr   p q) = 1 + max $ depth p $ depth q
+depth (XNOr  p q) = 1 + max $ depth p $ depth q
+depth (Imply p q) = 1 + max $ depth p $ depth q
+depth (Equiv p q) = 1 + max $ depth p $ depth q
