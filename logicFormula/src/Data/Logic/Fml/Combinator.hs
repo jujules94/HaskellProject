2813d987022bfaaf88798745dc8b2244fff9f1ee
Taeith <nils.pernet@gmail.com>
2020-11-27 18:03:44 +0100

    add multOr, multAdd

--- logicFormula/src/Data/Logic/Fml/Combinator.hs
+++ logicFormula/src/Data/Logic/Fml/Combinator.hs
@@ -2,7 +2,7 @@ module Data.Logic.Fml.Combinator (
   -- * Some functions on Fml
 
   multOr
-  --, multAnd
+  , multAnd
   --, allOf
   --, noneOf
   --, atLeast
@@ -16,6 +16,8 @@ module Data.Logic.Fml.Combinator (
   import qualified Data.Logic.Var      as Var
   import qualified Data.Logic.Fml      as Fml
 
+  import qualified Data.Logic.Var.Some as Vars
+
   -- |’multOr’ @fs@ returns the disjunction of the formulas in @fs.
   -- It returns @Nothing@ if @fs@ is the empty list.
   --
@@ -23,9 +25,13 @@ module Data.Logic.Fml.Combinator (
   -- Nothing
   -- >>> multOr [Fml.Final (Var.mk i) | i <- [1..4]]
   -- Just (Or (Final 1) (Or (Final 2) (Or (Final 3) (Final 4))))
-  multOr :: [Fml.Fml a] -> Maybe (Fml.Fml a)
+  multOr :: [Fml.Fml a] -> Maybe(Fml.Fml a)
   multOr [] = Nothing
-  multOr (x:xs) = Just x --totally wrong, for compilation purpose
+  multOr [x] = Just x
+  multOr (xs) = Just (helper xs)
+    where  
+      helper [x] = x
+      helper (x:xs) = Fml.Or x (helper xs)
 
   -- |’multAnd’ @fs@ returns the conjunction of the formulas in @fs.
   -- It returns @Nothing@ if @fs@ is the empty list.
@@ -34,7 +40,13 @@ module Data.Logic.Fml.Combinator (
   -- Nothing
   -- multAnd [Fml.Final (Var.mk i) | i <- [1..4]]
   -- Just (And (Final 1) (And (Final 2) (And (Final 3) (Final 4))))
-  -- multAnd :: [Fml.Fml a] -> Maybe (Fml.Fml a)
+  multAnd :: [Fml.Fml a] -> Maybe (Fml.Fml a)
+  multAnd [] = Nothing
+  multAnd [x] = Just x
+  multAnd (xs) = Just (helper xs)
+    where  
+      helper [x] = x
+      helper (x:xs) = Fml.And x (helper xs)
 
   -- |’allOf’ @vs@ returns a formula that is satisfiable iff all variables
   -- in @vs@ are true. The function returns @Nothing@ if @vs@ is the empty list.
