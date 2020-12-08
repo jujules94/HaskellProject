a1dad8b05dc347aa7b6cd182ce1db4c1fded8ea5
Taeith <nils.pernet@gmail.com>
2020-12-08 15:36:42 +0100

    fix

--- logicFormula/src/Data/Logic/Fml/Combinator.hs
+++ logicFormula/src/Data/Logic/Fml/Combinator.hs
@@ -1,72 +1,70 @@
 module Data.Logic.Fml.Combinator (
-  -- * Some functions on Fml
+-- * Some functions on Fml
 
   multOr
-  , multAnd
-  , allOf
-  , noneOf
-  --, atLeast
-  , atLeastOne
-  --, atMost
-  --, atMostOne
-  --, eaxctly
-  --, exactlyOne
+, multAnd
+, allOf
+, noneOf
+--, atLeast
+, atLeastOne
+--, atMost
+--, atMostOne
+--, exactly
+--, exactlyOne
 ) where
 
-  import qualified Data.Logic.Var      as Var
-  import qualified Data.Logic.Fml      as Fml
+import qualified Data.Logic.Var      as Var
+import qualified Data.Logic.Fml      as Fml
 
-  import qualified Data.Logic.Var.Some as Vars
+import qualified Data.Logic.Var.Some as Vars
 
-  -- |’multOr’ @fs@ returns the disjunction of the formulas in @fs.
-  -- It returns @Nothing@ if @fs@ is the empty list.
-  --
-  -- >>> Combinator.multOr []
-  -- Nothing
-  -- >>> multOr [Fml.Final (Var.mk i) | i <- [1..4]]
-  -- Just (Or (Final 1) (Or (Final 2) (Or (Final 3) (Final 4))))
-  multOr :: [Fml.Fml a] -> Maybe(Fml.Fml a)
-  multOr [] = Nothing
-  multOr [x] = Just x
-  multOr (xs) = Just (helper xs)
-    where  
-      helper [x] = x
-      helper (x:xs) = Fml.Or x (helper xs)
+-- |’multOr’ @fs@ returns the disjunction of the formulas in @fs.
+-- It returns @Nothing@ if @fs@ is the empty list.
+--
+-- >>> Combinator.multOr []
+-- Nothing
+-- >>> multOr [Fml.Final (Var.mk i) | i <- [1..4]]
+-- Just (Or (Final 1) (Or (Final 2) (Or (Final 3) (Final 4))))
+multOr :: [Fml.Fml a] -> Maybe(Fml.Fml a)
+multOr [] = Nothing
+multOr lst = Just (buildOr lst)
 
-  -- |’multAnd’ @fs@ returns the conjunction of the formulas in @fs.
-  -- It returns @Nothing@ if @fs@ is the empty list.
-  --
-  -- >>> Combinator.multAnd []
-  -- Nothing
-  -- multAnd [Fml.Final (Var.mk i) | i <- [1..4]]
-  -- Just (And (Final 1) (And (Final 2) (And (Final 3) (Final 4))))
-  multAnd :: [Fml.Fml a] -> Maybe (Fml.Fml a)
-  multAnd [] = Nothing
-  multAnd [x] = Just x
-  multAnd (xs) = Just (helper xs)
-    where  
-      helper [x] = x
-      helper (x:xs) = Fml.And x (helper xs)
+-- |’multAnd’ @fs@ returns the conjunction of the formulas in @fs.
+-- It returns @Nothing@ if @fs@ is the empty list.
+--
+-- >>> Combinator.multAnd []
+-- Nothing
+-- multAnd [Fml.Final (Var.mk i) | i <- [1..4]]
+-- Just (And (Final 1) (And (Final 2) (And (Final 3) (Final 4))))
+multAnd :: [Fml.Fml a] -> Maybe (Fml.Fml a)
+multAnd [] = Nothing
+multAnd lst = Just (buildAnd lst)
 
-  -- |’allOf’ @vs@ returns a formula that is satisfiable iff all variables
-  -- in @vs@ are true. The function returns @Nothing@ if @vs@ is the empty list.
-  allOf :: [Var.Var a] -> Maybe (Fml.Fml a)
-  allOf []  = Nothing
-  allOf [x] = Just (Fml.Final x)
-  allOf (xs) = Just (helper xs)
-    where
-        helper [x] = Fml.Final x
-        helper (x:xs) = Fml.And (Fml.Final x) (helper xs)
+-- |buildAnd is used to join the fmls with And (like in CCNF)
+buildAnd :: [Fml.Fml a] -> Fml.Fml a
+buildAnd lst = f lst
+  where
+    f [x]    = x
+    f (x:xs) = Fml.And x $ f xs
 
-  -- |’noneOf’ @vs@ returns a formula that is satisfiable iff no variable
-  -- in @vs@ is true. The function returns @Nothing@ if @vs@ is the empty list.
-  noneOf :: [Var.Var a] -> Maybe (Fml.Fml a)
-  noneOf []  = Nothing
-  noneOf [x] = Just (Fml.Not (Fml.Final x))
-  noneOf (xs) = Just (helper xs)
-    where
-        helper [x] = Fml.Not (Fml.Final x)
-        helper (x:xs) = Fml.And (Fml.Not (Fml.Final x)) (helper xs)
+-- |buildOr is used to join the fmls with Or
+buildOr :: [Fml.Fml a] -> Fml.Fml a
+buildOr lst = f lst
+  where
+    f [x]    = x
+    f (x:xs) = Fml.Or x $ f xs
+
+-- |’allOf’ @vs@ returns a formula that is satisfiable iff all variables
+-- in @vs@ are true. The function returns @Nothing@ if @vs@ is the empty list.
+allOf :: [Var.Var a] -> Maybe (Fml.Fml a)
+allOf []  = Nothing
+allOf lst = Just (buildAnd [Fml.Final elem | elem <- lst])
+
+-- |’noneOf’ @vs@ returns a formula that is satisfiable iff no variable
+-- in @vs@ is true. The function returns @Nothing@ if @vs@ is the empty list.
+noneOf :: [Var.Var a] -> Maybe (Fml.Fml a)
+noneOf []  = Nothing
+noneOf lst = Just (buildAnd [Fml.Not (Fml.Final elem) | elem <- lst])
 
   -- |’atLeast’ @vs@ @k@ returns a formula that is satisfied iff at least @k@
   -- variables in @vs@ are true. The function returns @Nothing@ if @vs@ is the
