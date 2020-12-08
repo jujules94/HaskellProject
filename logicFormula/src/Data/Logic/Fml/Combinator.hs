2b8f9460f0a9e252c83afc14bd6d6a20a8756b45
jbessode <jbessode@etud.u-pem.fr>
2020-12-08 15:39:27 +0100

    work combinator with binome

--- logicFormula/src/Data/Logic/Fml/Combinator.hs
+++ logicFormula/src/Data/Logic/Fml/Combinator.hs
@@ -5,12 +5,12 @@ module Data.Logic.Fml.Combinator (
 , multAnd
 , allOf
 , noneOf
---, atLeast
+, atLeast
 , atLeastOne
---, atMost
---, atMostOne
---, exactly
---, exactlyOne
+, atMost
+, atMostOne
+, exactly
+, exactlyOne
 ) where
 
 import qualified Data.Logic.Var      as Var
@@ -66,41 +66,78 @@ noneOf :: [Var.Var a] -> Maybe (Fml.Fml a)
 noneOf []  = Nothing
 noneOf lst = Just (buildAnd [Fml.Not (Fml.Final elem) | elem <- lst])
 
-  -- |’atLeast’ @vs@ @k@ returns a formula that is satisfied iff at least @k@
-  -- variables in @vs@ are true. The function returns @Nothing@ if @vs@ is the
-  -- empty list or @k@ is non-positive or @k@ is larger than the number of
-  -- variables in @vs@.
-  -- atLeast :: [Var.Var a] -> Int -> Maybe (Fml.Fml a)
+ 
+-- |’atLeast’ @vs@ @k@ returns a formula that is satisfied iff at least @k@
+-- variables in @vs@ are true. The function returns @Nothing@ if @vs@ is the
+-- empty list or @k@ is non-positive or @k@ is larger than the number of
+-- variables in @vs@.
+atLeast :: [Var.Var a] -> Int -> Maybe (Fml.Fml a)
+atLeast [] _ = Nothing
+atLeast _  0 = Nothing
+atLeast lst n
+  |  n > length lst = Nothing
+  |  otherwise = Just (buildOr [buildAnd fml | fml <- fmls])
+       where
+         fmls = buildFmlsFromIndices (computeIndices 0 (length lst - 1) n []) [Fml.Final elem | elem <- lst]
 
-  -- |’atLeastOne’ @vs@ returns a formula that is satisfiable iff at least one
-  -- variable in @vs@ is true. The function returns @Nothing@ if @vs@ is the
-  -- empty list.
-  atLeastOne :: [Var.Var a] -> Maybe (Fml.Fml a)
-  atLeastOne []   = Nothing
-  atLeastOne [x]  = Just (Fml.Final x)
-  atLeastOne (xs) = Just (helper xs)
-    where
-      helper [x] = Fml.Final x
-      helper (x:xs) = Fml.Or (Fml.Final x) (helper xs)
+-- |’atLeastOne’ @vs@ returns a formula that is satisfiable iff at least one
+-- variable in @vs@ is true. The function returns @Nothing@ if @vs@ is the
+-- empty list.
+atLeastOne :: [Var.Var a] -> Maybe (Fml.Fml a)
+atLeastOne vars = atLeast vars 1
 
-  -- |’atMost’ @vs@ @k@ returns a formula that is satisfiable iff at most @k@
-  -- variables in @vs@ are true. The function returns @Nothing@ if @vs@ is the
-  -- empty list or @k@ is non-positive or @k@ is larger than the number of
-  -- variables in @vs@.
-  -- atMost :: [Var.Var a] -> Int -> Maybe (Fml.Fml a)
+-- |’atMost’ @vs@ @k@ returns a formula that is satisfiable iff at most @k@
+-- variables in @vs@ are true. The function returns @Nothing@ if @vs@ is the
+-- empty list or @k@ is non-positive or @k@ is larger than the number of
+-- variables in @vs@.
+atMost :: [Var.Var a] -> Int -> Maybe (Fml.Fml a)
+atMost [] _ = Nothing
+atMost _  0 = Nothing
+atMost lst n
+  |  n > length lst = Nothing
+  |  otherwise = Just (buildOr [buildAnd fml | fml <- fmls])
+       where
+         fmls = buildFmlsFromIndices (computeIndices 0 (length lst - 1) (length lst - n) []) [Fml.Not (Fml.Final elem) | elem <- lst]
 
-  -- |’atMostOne’ @vs@ returns a formula that is satisfiable iff at most one
-  -- variable in @vs@ is true. The function returns @Nothing@ if @vs@ is the
-  -- empty list.
-  -- atMostOne :: [Var.Var a] -> Maybe (Fml.Fml a)
+-- |’atMostOne’ @vs@ returns a formula that is satisfiable iff at most one
+-- variable in @vs@ is true. The function returns @Nothing@ if @vs@ is the
+-- empty list.
+atMostOne :: [Var.Var a] -> Maybe (Fml.Fml a)
+atMostOne vars = atMost vars 1
 
-  -- |’exactly’ @vs@ @k@ returns a formula that is satisfiable iff exactly @k@
-  -- variables in @vs@ are true. The function returns @Nothing@ if @vs@ is the
-  -- empty list or @k@ is non-positive or @k@ is larger than the number of
-  -- variables in @vs@.
-  -- exactly :: [Var.Var a] -> Int -> Maybe (Fml.Fml a)
+-- |’exactly’ @vs@ @k@ returns a formula that is satisfiable iff exactly @k@
+-- variables in @vs@ are true. The function returns @Nothing@ if @vs@ is the
+-- empty list or @k@ is non-positive or @k@ is larger than the number of
+-- variables in @vs@.
+exactly :: [Var.Var a] -> Int -> Maybe (Fml.Fml a)
+exactly [] _ = Nothing
+exactly _  0 = Nothing
+exactly lst n
+  |  n > length lst = Nothing
+  |  otherwise = f least most
+       where
+         least = atLeast lst n
+         most  = atMost  lst n
+         f Nothing Nothing   = Nothing
+         f Nothing (Just m)  = Just(m)
+         f (Just l) Nothing  = Just(l)
+         f (Just l) (Just m) = Just(Fml.And l m)
 
-  -- |’exactlyOne’ @vs@ returns a formula that is satisfiable iff exactly one
-  -- variable in @vs@ is true. The function returns @Nothing@ if @vs@ is the
-  -- empty list.
-  -- exactlyOne :: [Var.Var a] -> Maybe (Fml.Fml a)
+-- |’exactlyOne’ @vs@ returns a formula that is satisfiable iff exactly one
+-- variable in @vs@ is true. The function returns @Nothing@ if @vs@ is the
+-- empty list.
+exactlyOne :: [Var.Var a] -> Maybe (Fml.Fml a)
+exactlyOne vars = exactly vars 1
+
+-- |’computeIndices’ return a list of list of indices in order to compute all 
+-- the increasing sequences of size n between a min and max value
+computeIndices :: Int -> Int -> Int -> [Int] -> [[Int]]
+computeIndices _ _ 0 _ = []
+computeIndices _ 0 _ _ = []
+computeIndices min max 1 acc = [acc ++ [i] | i <- [min..max]]
+computeIndices min max quantity acc
+  |  min == max = []
+  |  otherwise = computeIndices (min+1) max (quantity-1) (acc ++ [min]) ++ computeIndices (min+1) max quantity acc
+
+buildFmlsFromIndices :: [[Int]] -> [Fml.Fml a] -> [[Fml.Fml a]]
+buildFmlsFromIndices indices fmls = [[fmls !! i | i <- ind]| ind <- indices]
